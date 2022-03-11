e96d1458985fb3f0b43a6dad1261786fd0d2f7f5
Judgement9882 <kjy6135@inu.ac.kr>
2022-03-11 10:51:22 +0900

    add annotation
    add annotation

--- micropython-mask-recognition.py
+++ micropython-mask-recognition.py
@@ -10,14 +10,15 @@ msg_byte = uart.read()
 display.scroll(str(msg_byte)) # print None
 
 while True:
-    msg_byte = uart.read()
-    msg_str = str(msg_byte)
+    msg_byte = uart.read() # bytes type
+    msg_str = str(msg_byte) # bytes -> str
 
+# output : Mask or NoMask
     if msg_str[2:6] == 'Mask':
         display.show(Image.HAPPY)
     if msg_str[2:8] == 'NoMask':
         display.show(Image.NO)
-        pin1.write_digital(1)
-        music.play(tune)
-        sleep(1000)
-        pin1.write_digital(0)
+        pin1.write_digital(1) # led on
+        music.play(tune) # warning sign
+        sleep(1000) 
+        pin1.write_digital(0) # led off
